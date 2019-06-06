a0b6cd86c68a5f25a4f0f9f630bd4bc3e90f54a4
Łukasz Kliś <lukasz@klis.pl>
2019-06-06 23:58:35 +0200

    Add BIPoCiT Space team portraits (#152)
    * Add BIPoCiT Space team portraits
    
    * Use LTS node.js version
    
    * Check for team members presence

--- plugins/nunjucks.js
+++ plugins/nunjucks.js
@@ -30,6 +30,19 @@ module.exports = function(env, callback) {
     );
   };
 
+  env.config.locals.filterTeamByResponsibility = function(
+    teamMembers,
+    responsibility
+  ) {
+    if (!teamMembers) {
+      return [];
+    }
+
+    return teamMembers.filter(teamMember =>
+      teamMember.metadata.team.tags.includes(responsibility)
+    );
+  };
+
   env.config.locals.Date = Date;
 
   // Load the new nunjucks environment.
